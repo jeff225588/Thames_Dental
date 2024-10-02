b9956f679e36493575a9888f10794434a34ffa55
David Gómez <dagove47@gmail.com>
2024-10-02 02:24:05 -0600

    Admin Controller added

---
+++ Thames_Dental_Web/Thames_Dental_Web/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Thames_Dental_Web.Models;
+
+namespace Thames_Dental_Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
