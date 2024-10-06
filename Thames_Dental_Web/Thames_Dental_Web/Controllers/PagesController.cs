dd11a011ae0b953c644cda319d5bd0dacbd66258
David Gómez <dagove47@gmail.com>
2024-10-06 17:15:46 -0600

    Main user pages
    Added:
    - Index
    - Servicios
    - Nosotros
    - Equipo
    - Contacto
    - js libs and files
    - css files

---
+++ Thames_Dental_Web/Thames_Dental_Web/Controllers/PagesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Thames_Dental_Web.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Servicios()
+        {
+            return View();
+        }
+
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
+        public IActionResult Equipo()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+    }
+}
