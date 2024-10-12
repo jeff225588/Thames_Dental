d5013894a47d8a471197522287506ef873db18f0
David Gómez <dagove47@gmail.com>
2024-10-11 23:12:00 -0600

    Expedientes Controller and views added
    Views:
    - Autorizados.
    - Expedientes.
    - Historia Clinica.
    - Laboratorio.
    - Notas.
    - Odontograma.
    - Recetario.

---
+++ Thames_Dental_Web/Thames_Dental_Web/Controllers/ExpedientesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Thames_Dental_Web.Controllers
+{
+    public class ExpedientesController : Controller
+    {
+        public IActionResult Expediente()
+        {
+            return View();
+        }
+
+        public IActionResult Autorizados()
+        {
+            return View();
+        }
+
+        public IActionResult Notas()
+        {
+            return View();
+        }
+
+        public IActionResult HistoriaClinica()
+        {
+            return View();
+        }
+
+        public IActionResult Odontograma()
+        {
+            return View();
+        }
+
+        public IActionResult Recetario()
+        {
+            return View();
+        }
+
+        public IActionResult Laboratorio()
+        {
+            return View();
+        }
+    }
+}
