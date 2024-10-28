4f4a7991b2b7f2222c5c1d03002ed9f820fb4202
David Gómez <dagove47@gmail.com>
2024-10-28 02:01:32 -0600

    Autenticacion views updated

--- Thames_Dental_Web/Thames_Dental_Web/Controllers/AutenticacionController.cs
+++ Thames_Dental_Web/Thames_Dental_Web/Controllers/AutenticacionController.cs
@@ -18,17 +18,41 @@ namespace Thames_Dental_Web.Controllers
             return View();
         }
 
+
+
+        [HttpGet]
         public IActionResult RegistrarAdmin()
         {
             return View();
         }
-
-        public IActionResult Ingresar()
+        [HttpPost]
+        public IActionResult RegistrarAdmin(UsuarioModel model)
         {
             return View();
         }
 
+
+
+        [HttpGet]
+        public IActionResult Ingresar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Ingresar(UsuarioModel model)
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
         public IActionResult RecuperarContrasena()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RecuperarContrasena(UsuarioModel model)
         {
             return View();
         }
